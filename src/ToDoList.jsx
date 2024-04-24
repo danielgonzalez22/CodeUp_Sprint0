4682eb273e70a00eb54672afc81b47e25d4b67ae
danielgonzalez22 <kavellid63@gmail.com>
2024-04-24 15:55:06 -0300

    functions boilerplate

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,47 +1,87 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 function ToDoList() {
 
-    const [tasks, setTasks] = useState(["comprar Diclorodifeniltricloroetano", "comprar pan", "sacar la basura", "limpiar el espectrómetro de resonancia magnética nuclear de alto campo"])
-    const [newTask, setNewTask] = useState("")
+  const [tasks, setTasks] = useState(["comprar Diclorodifeniltricloroetano", "comprar pan", "sacar la basura", "limpiar el espectrómetro de resonancia magnética nuclear de alto campo"])
+  const [newTask, setNewTask] = useState("")
 
-    function handleInputChange(event) {
-        setNewTask(event.target.value)
-    }
+  useEffect(()=>{
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+    console.log(tasks)
+  }, [tasks])
 
+  function handleNewTaskInput(event) {
+    setNewTask(event.target.value)
+  }
 
-    function addTask() {
+  function handleAddTaskBtn() {
 
-    }
-
-
-    function deleteTask(listItemIndex) {
-
-    }
-
-
-    return (
-        <div className="to-do-list">
-            <h1>To-Do-List</h1>
-
-            <div>
-                <input
-                    type="text"
-                    placeholder='Write a task...'
-                    value={newTask}
-                    onChange={handleInputChange}
-                />
-                <button
-                className='btn-add'
-                onClick={addTask}
-                >
-                </button>
-            </div>
+  }
 
 
+  function handleSearchEmptyBtn() {
 
-        </div>
-    )
+  }
 
+
+  function handleSearchInput(event) {
+    if (!event.target.value) return setTasks(tasks)
+    const resultsArray = tasks.filter(task => task.includes(event.target.value))
+    setTasks(resultsArray)
+  }
+
+
+  function addTask() {
+
+  }
+
+
+  function deleteTask(listItemIndex) {
+    setTasks(tasks.filter((task, index) => index !== listItemIndex))
+  }
+
+
+
+  return (
+    <div className="to-do-list">
+      <h1>To-Do-List</h1>
+      <div>
+        <input
+          type="text"
+          placeholder='New task...'
+          value={newTask}
+          onChange={handleNewTaskInput}
+        />
+        <button
+          className='btn-add'
+          onClick={addTask}
+        >
+        </button>
+        <input
+          type="text"
+          placeholder='Search task...'
+          onChange={handleSearchInput}
+        />
+        <button
+          className='btn-empty-search'
+          onClick={handleSearchEmptyBtn}
+        >
+        </button>
+      </div>
+      <ol>
+        {tasks.map((task, index) =>
+          <li key={index}>
+            <span className='li-text'>{task}</span>
+            <button
+              className='btn-delete'
+              onClick={() => deleteTask(index)}
+            >
+              Delete task
+            </button>
+          </li>
+        )}
+      </ol>
+    </div>
+  )
 }
 export default ToDoList
